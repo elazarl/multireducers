0fe61b5cecc8008b39a8fe6d72023c8cd6cca502
Elazar Leibovich <elazarl@gmail.com>
2014-10-27 16:44:23 +0200

    add dummy value, so that every map will contain something

--- src/main/java/com/akamai/csi/multireducers/MapToProperties.java
+++ src/main/java/com/akamai/csi/multireducers/MapToProperties.java
@@ -17,23 +17,32 @@ import java.util.Map;
 public class MapToProperties {
 
     private static String utf8 = Charsets.UTF_8.name();
+    private static String dummy = "com.akamai.csi.multireducers.mulitoutputformat.placeholder";
 
     static Map<String, String> deserialize(String s) {
         Map<String, String> m = Maps.newHashMap();
         String[] keyvals = s.split(";");
         for (String keyval : keyvals) {
             String[] parts = keyval.split("=");
+            String key = parts[0];
+            String value = "";
+            if (parts.length > 1) {
+                value = parts[1];
+            }
             try {
-                m.put(URLDecoder.decode(parts[0], utf8), URLDecoder.decode(parts[1], utf8));
+                m.put(URLDecoder.decode(key, utf8), URLDecoder.decode(value, utf8));
             } catch (UnsupportedEncodingException e) {
                 throw new RuntimeException(e);
             }
         }
+        m.remove(dummy);
         return m;
     }
 
     static String serialize(Map<String, String> m) {
         List<String> parts = Lists.newArrayList();
+        // This is to ensure we never return the empty string
+        parts.add(dummy + "=");
         for (Map.Entry<String, String> entry : m.entrySet()) {
             try {
                 String key = URLEncoder.encode(entry.getKey(), utf8);
